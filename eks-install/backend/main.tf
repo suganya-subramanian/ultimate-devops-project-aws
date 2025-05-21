525bbc54b34e2bd6e4c8c4fb248db1aa91f06a72
suganya s <Ssuganya974@gmail.com>
2025-05-21 07:33:13 +0530

    change s3 , dynamodb name

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "demo-terraform-eks-state-s3-bucket"
+  bucket = "suganya-demo-terraform-eks-state-s3-bucket"
 
   lifecycle {
     prevent_destroy = false
@@ -28,7 +28,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-eks-state-locks"
+  name         = "suganya-terraform-eks-state-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
